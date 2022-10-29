3d0d4fc680dd21fccc8288f86b24d2ee7c825703
ecdestro <61299394+ecdestro@users.noreply.github.com>
2022-10-29 02:59:31 -0500

    updated frontend and backend

--- frontend.py
+++ frontend.py
@@ -1,42 +1,47 @@
 from tkinter import *
-import backend
+import db_backend as db_backend
 
 def get_selected_row(event):
     try:
-        global selected_tuple
         index=listMain.curselection()[0]
         selected_tuple = listMain.get(index)
         pNameText.delete(0, END)
         pNameText.insert(END, selected_tuple[1])
-        pExpVal.delete(0, END)
-        pExpVal.insert(END, selected_tuple[2])
+        pXpVal.delete(0, END)
+        pXpVal.insert(END, selected_tuple[2])
         pHPVal.delete(0, END)
         pHPVal.insert(END, selected_tuple[3])
         pGoldVal.delete(0, END)
         pGoldVal.insert(END, selected_tuple[4])
+        pDmgVal.delete(0, END)
+        pDmgVal.insert(END, selected_tuple[5])
     except IndexError:
         pass
 
 def viewCommand():
     listMain.delete(0,END)
-    for row in backend.view():
+    for row in db_backend.view():
         listMain.insert(END, row)
 
 def searchPatrons():
     listMain.delete(0,END)
-    for row in backend.search(patronName.get(),patronExp.get(),patronHP.get(),patronGold.get()):
+    for row in db_backend.search(patronName.get(),patronXp.get(),patronHP.get(),patronGold.get(),patronDmg.get()):
         listMain.insert(END, row)
 
 def addPatron():
-    backend.insert(patronName.get(),patronExp.get(),patronHP.get(),patronGold.get())
+    db_backend.insert(patronName.get(),patronXp.get(),patronHP.get(),patronGold.get(),patronDmg.get())
     listMain.delete(0, END)
-    listMain.insert(END,(patronName.get(),patronExp.get(),patronHP.get(),patronGold.get()))
+    listMain.insert(END,(patronName.get(),patronXp.get(),patronHP.get(),patronGold.get()),patronDmg.get())
 
 def updatePatron():
-    backend.update(selected_tuple[0],patronName.get(),patronExp.get(),patronHP.get(),patronGold.get())
+    index=listMain.curselection()[0]
+    selected_tuple = listMain.get(index)
+    db_backend.update(selected_tuple[0],patronName.get(),patronXp.get(),patronHP.get(),patronGold.get(),patronDmg.get())
 
 def deletePatron():
-    backend.delete(selected_tuple[0])
+    index=listMain.curselection()[0]
+    selected_tuple = listMain.get(index)
+    db_backend.delete(selected_tuple[0])
 
 window = Tk()
 window.title("Database")
@@ -44,57 +49,68 @@ window.title("Database")
 pName = Label(window, text="Name")
 pName.grid(row = 0, column = 0)
 
-pExp = Label(window, text="Exp")
-pExp.grid(row = 0, column = 2)
+pXp = Label(window, text="Xp")
+pXp.grid(row = 1, column = 0)
 
 pHP = Label(window, text="HP")
-pHP.grid(row = 1, column = 0)
+pHP.grid(row = 1, column = 2)
 
 pGold = Label(window, text="Gold")
-pGold.grid(row = 1, column = 2)
+pGold.grid(row = 2, column = 0)
+
+pDmg = Label(window, text="Damage")
+pDmg.grid(row=2, column=2)
 
 patronName = StringVar()
 pNameText = Entry(window, textvariable=patronName)
-pNameText.grid(row=0, column=1)
+pNameText.grid(row=0, column=1, columnspan=3)
 
-patronExp = IntVar()
-pExpVal = Entry(window, textvariable=patronExp)
-pExpVal.grid(row=0, column=3)
+patronXp = IntVar()
+pXpVal = Entry(window, textvariable=patronXp)
+pXpVal.grid(row=1, column=1)
 
 patronHP = IntVar()
 pHPVal = Entry(window, textvariable=patronHP)
-pHPVal.grid(row=1, column=1)
+pHPVal.grid(row=1, column=3)
 
 patronGold = IntVar()
 pGoldVal = Entry(window, textvariable=patronGold)
-pGoldVal.grid(row=1, column=3)
+pGoldVal.grid(row=2, column=1)
+
+patronDmg = IntVar()
+pDmgVal = Entry(window, textvariable=patronDmg)
+pDmgVal.grid(row=2, column=3)
 
 listMain = Listbox(window, height=10, width=35)
-listMain.grid(row=2, column=0, rowspan=8, columnspan=2)
+listMain.grid(row=3, column=0, rowspan=8, columnspan=2)
+listMain.delete(0,END)
+for row in db_backend.view():
+    listMain.insert(END, row)
 
 yScroll = Scrollbar(window)
-yScroll.grid(row=2, column=2, rowspan=4)
+yScroll.grid(row=3, column=2, rowspan=6)
 
 listMain.configure(yscrollcommand=yScroll.set)
 yScroll.configure(command=listMain.yview)
 
+listMain.bind('<<ListboxSelect>>', get_selected_row)
+
 bViewAll = Button(window, text="View All", width=12, command=viewCommand)
-bViewAll.grid(row=2, column= 3)
+bViewAll.grid(row=3, column= 3)
 
 bSearch = Button(window, text="Search", width=12, command=searchPatrons)
-bSearch.grid(row=3, column= 3)
+bSearch.grid(row=4, column= 3)
 
 bNew = Button(window, text="New", width=12, command=addPatron)
-bNew.grid(row=4, column= 3)
+bNew.grid(row=5, column= 3)
 
 bUpdate = Button(window, text="Update", width=12, command=updatePatron)
-bUpdate.grid(row=5, column= 3)
+bUpdate.grid(row=6, column= 3)
 
 bDelete = Button(window, text="Delete", width=12, command=deletePatron)
-bDelete.grid(row=6, column= 3)
+bDelete.grid(row=7, column= 3)
 
 bClose = Button(window, text="Close", width=12, command=window.destroy)
-bClose.grid(row=7, column= 3)
+bClose.grid(row=8, column= 3)
 
 window.mainloop()
-
